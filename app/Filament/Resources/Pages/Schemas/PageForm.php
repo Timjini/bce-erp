b3a0600a4b3bd225a895af303ad9cd8084115b83
Timjini <hatim.jini@gmail.com>
2025-08-20 22:34:08 +0200

    bucket setup

--- app/Filament/Resources/Pages/Schemas/PageForm.php
+++ app/Filament/Resources/Pages/Schemas/PageForm.php
@@ -33,6 +33,13 @@ class PageForm
                     ->required()
                     ->maxLength(255)
                     ->unique(Page::class, 'slug'),
+                
+                Forms\Components\FileUpload::make('image_url')
+                ->label('Page Image')
+                ->image() // ensures only image files
+                ->directory('pages') // uploaded to storage/app/public/pages or S3 bucket depending on config
+                ->disk('s3') // or 'public'
+                ->nullable(),
 
                 // Forms\Components\RichEditor::make('content')
                 // ->label('Content')
